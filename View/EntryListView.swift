96cf347d0942849fa6172464ddfe8008570a76e1
Plus1XP <evldev@protonmail.ch>
2022-05-24 14:36:51 +0100

    Feat(View): Add calendar search to list view

--- View/EntryListView.swift
+++ View/EntryListView.swift
@@ -10,23 +10,32 @@ import CoreData
 
 struct EntryListView: View {
     @ObservedObject var entryStore: EntryStore
-    @State private var entry: Entry
+    @State private var entry: Entry = Entry(context: PersistenceController.shared.container.viewContext)
     @State private var canShowAddEntryView: Bool = false
     @State private var canShowEditEntryView: Bool = false
+    @State private var canAutoCompleteSearch: Bool = false
+    @State private var canResetDate: Bool = false
+    @State private var isSearchingDate: Bool = false
     @State private var hasEntrySaved: Bool = false
     @State private var searchQuery: String = ""
+    @State private var searchDate: Date = Date.now
+    @State private var calendarId: UUID = UUID()
     
     private var searchResults: [Entry] {
-        if searchQuery.isEmpty {
-            return entryStore.entries
-        } else {
+        if !searchQuery.isEmpty {
             return entryStore.entries.filter { $0.event!.localizedCaseInsensitiveContains(searchQuery) }
+        }
+        if isSearchingDate {
+            return entryStore.entries.filter { Calendar.current.dateComponents([.year, .month, .day], from: $0.timestamp!) == Calendar.current.dateComponents([.year, .month, .day], from: searchDate) }
+        }
+        else {
+            return entryStore.entries
         }
     }
 
     init(entryStore: EntryStore) {
         self.entryStore = entryStore
-        self.entry = Entry(context: PersistenceController.shared.container.viewContext)
+//        self.entry = Entry(context: PersistenceController.shared.container.viewContext)
         PersistenceController.shared.container.viewContext.delete(entry)
     }
     
@@ -59,15 +68,46 @@ struct EntryListView: View {
         }
         .navigationTitle("Emojions")
         .searchable(text: $searchQuery, prompt: "Search Emojions") {
-//            if searchQuery.count > 2 {
-//                ForEach(searchResults, id: \.self) { entry in
-//                    Text(entry.event!).searchCompletion(entry.event!)
-//                }
-//            }
+            if canAutoCompleteSearch && searchQuery.count > 2 {
+                ForEach(searchResults, id: \.self) { entry in
+                    Text(entry.event!).searchCompletion(entry.event!)
+                }
+            }
         }
         .disableAutocorrection(true)
         .toolbar {
-            ToolbarItem {
+            ToolbarItem(placement: .navigationBarLeading) {
+                HStack {
+                    DatePicker("Please enter a date", selection: $searchDate, displayedComponents: .date)
+                        .labelsHidden()
+                        .id(calendarId)
+                        // Needed to close calendar picker after selection
+                        .onChange(of: Calendar.current.component(.day, from: searchDate)) { _ in
+                            calendarId = UUID()
+                            if !canResetDate {
+                                isSearchingDate = true
+                            } else {
+                                canResetDate = false
+                            }
+                        }
+                    Button(action: {
+                        if isSearchingDate {
+                            isSearchingDate = false
+                            canResetDate = true
+                            searchDate = Date.now
+                        }
+                    }) {
+                        Label("Reset Calendar", systemImage: "xmark.circle.fill")
+                            .foregroundColor(Color.red)
+                            .disabled(!isSearchingDate)
+                            .opacity(isSearchingDate ? 1 : 0)
+                    }
+
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
                 Button(action: {
                     self.canShowAddEntryView.toggle()
                 }) {
